db4d0bc34520792014e4d65d5605bd027e18f0b5
barrie <barrie@reboot-robot.net>
2016-07-04 18:11:23 +0100

    No longer prints "found" for IPs which match the wildcard record

--- fierce.py
+++ fierce.py
@@ -240,7 +240,7 @@ def fierce(**kwargs):
         url = concatenate_subdomains(domain, [subdomain])
         record = query(resolver, url, record_type='A')
 
-        if record is None:
+        if record is None or (record[0].address == wildcard[0].address):
             continue
 
         ip = ipaddress.IPv4Address(record[0].address)
