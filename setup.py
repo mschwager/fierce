e813c80409728c94fb44dcad3e496815c669e3a2
Matt Schwager <schwag09@gmail.com>
2018-04-24 14:53:19 -0400

    Add Python 3 requirement to setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     ],
     install_requires=install_requires,
     tests_require=tests_require,
+    python_requires='>=3.0',
     entry_points={
         'console_scripts': [
             'fierce = fierce.fierce:main',
